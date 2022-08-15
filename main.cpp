eb2504f78c253f00467bdd3700790e8a56ee2555
chinnihoney <chinni.honey333@gmail.com>
2022-08-15 07:47:17 +0530

    sum of vector elements

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 int main()
 {
     vector<int> v1={1,2,8,4,7,3};
+    int sum=0;
+
     cout<<"{";
-    for_each(v1.begin(),v1.end(),[](const auto &x){cout<<x;});
+    for_each(v1.begin(),v1.end(),[&sum](const auto &x){cout<<x<<"";sum+=x;});
     cout<<"}";
+    cout<<"sum of vector elements"<<sum<<endl;
     return 0;
     
 
